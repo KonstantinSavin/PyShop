6aa314858cfa3ec414be6fc84541e2abaab2ac52
Konstantin Savin <110243772+KonstantinSavin@users.noreply.github.com>
2024-09-18 19:04:38 +0300

    Fix ex3

--- e3/main.go
+++ e3/main.go
@@ -11,88 +11,91 @@ import (
 	"time"
 )
 
-type arrRange []string
+type arrRangeStr []string
 
-func (i *arrRange) String() string {
+func (i *arrRangeStr) String() string {
 	return ""
 }
 
-func (i *arrRange) Set(value string) error {
+func (i *arrRangeStr) Set(value string) error {
 	*i = append(*i, strings.TrimSpace(value))
 	return nil
 }
 
-func findPrimes(s string, ch chan<- []int) {
+func rangeToArr(rangeStr string) (arrRangeInt [2]int) {
+	ss := strings.Split(rangeStr, ":")
 
-	ss := strings.Split(s, ":")
-
-	var arrRange = [2]int{}
 	var err error
-	arrRange[0], err = strconv.Atoi(ss[0])
+	arrRangeInt[0], err = strconv.Atoi(ss[0])
 	if err != nil {
-		fmt.Println("Неверный формат диапазона", err)
+		fmt.Println("rangeToArr -> Неверный формат диапазона", err)
 		os.Exit(1)
 	}
 
-	arrRange[1], err = strconv.Atoi(ss[1])
+	arrRangeInt[1], err = strconv.Atoi(ss[1])
 	if err != nil {
-		fmt.Println("Неверный формат диапазона", err)
+		fmt.Println("rangeToArr -> Неверный формат диапазона", err)
 		os.Exit(1)
 	}
 
+	fmt.Println("rangeToArr -> arrRange: ", arrRangeInt)
+
+	return arrRangeInt
+}
+
+func findPrimes(ctx context.Context, arrRangeInt [2]int, ch chan<- []int) {
 	primes := []int{}
-	for i := arrRange[0]; i < arrRange[1]; i++ {
+	for i := arrRangeInt[0]; i <= arrRangeInt[1]; i++ {
 		if big.NewInt(int64(i)).ProbablyPrime(0) {
 			primes = append(primes, i)
 		}
 	}
+
+	fmt.Println("findPrimes -> primes: ", primes)
+
 	ch <- primes
 }
 
-func writeFile(file os.File, ch <-chan [][]int) {
+func writeFile(ctx context.Context, filename string, arrRangeInt [2]int, ch <-chan []int) {
 	primes := <-ch
+	fmt.Println("writeFile -> primes:", primes)
 
-	file.
-	defer file.Close()
-
-	for i := 0; i < len(primes); i++ {
-		file.WriteString(fmt.Sprintf("Простые числа из диапазона %d:\n", i+1))
-		file.WriteString(strings.Trim(strings.Join(strings.Split(fmt.Sprint(primes[i]), " "), ","), "[]"))
-
-		file.WriteString("\n")
-	}
-}
-
-func main() {
-	filename := flag.String("file", "", "имя файла для вывода найденных простых чисел")
-	timeout := *flag.Int("timeout", 0, "значение в секундах, по истечении которого программа должна прекратить свое исполнение")
-	var ranges arrRange
-	flag.Var(&ranges, "range", "диапазоны чисел, в пределах которого программа должна найти простые числа")
-
-	flag.Parse()
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
-
-	file, err := os.Create(*filename)
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println("Невозможно создать файл:", err)
+		fmt.Println("writeFile -> Невозможно создать файл:", err)
 		os.Exit(1)
 	}
 	defer file.Close()
 
+	file.WriteString(fmt.Sprintf("Простые числа из диапазона %d:%d это %s\n", arrRangeInt[0], arrRangeInt[1], strings.Trim(strings.Join(strings.Split(fmt.Sprint(primes), " "), ", "), "[]")))
+}
+
+func main() {
+	filename := flag.String("file", "", "имя файла для вывода найденных простых чисел")
+	timeout := flag.Int("timeout", 0, "значение в секундах, по истечении которого программа должна прекратить свое исполнение")
+	var ranges arrRangeStr
+	flag.Var(&ranges, "range", "диапазоны чисел, в пределах которого программа должна найти простые числа")
+
+	flag.Parse()
+
+	fmt.Println("filename: ", *filename)
+	fmt.Println("timeout: ", *timeout)
+	fmt.Println("ranges: ", ranges)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout)*time.Second)
+
+	defer func() {
+		cancel()
+		fmt.Println("Main Defer: canceling context")
+	}()
+
 	primeCh := make(chan []int)
-	allPrimesCh := make(chan [][]int)
 
-	go writeFile(*filename, allPrimesCh)
-
-	var primes [][]int
 	for _, v := range ranges {
-		go findPrimes(v, primeCh)
-		primes = append(primes, <-primeCh)
+		arrRangeInt := rangeToArr(v)
+		go writeFile(ctx, *filename, arrRangeInt, primeCh)
+		go findPrimes(ctx, arrRangeInt, primeCh)
 	}
-	fmt.Println(primes)
 
-	allPrimesCh <- primes
-
-	// time.Sleep(time.Second * 3)
+	time.Sleep(time.Second * 10)
 }
